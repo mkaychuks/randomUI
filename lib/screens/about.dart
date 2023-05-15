a4de2c83046d492d1f3d63fb34588cc9a2873022
mkaychuks <sachufusi@gmail.com>
2023-05-15 10:30:57 +0100

    stateProvider and its usecase

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,22 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:learn_riverpod/provider/basic_provider.dart';
+import 'package:learn_riverpod/provider/state_provider.dart';
 
 class AboutPage extends ConsumerWidget {
   const AboutPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final genList = ref.watch(namesProvider);
+    final value = ref.watch(currentStateProvider);
+
+    ref.listen<int>(
+      currentStateProvider,
+      (prev, curr) {
+        if (curr == 65) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text("Value is 65"),
+            ),
+          );
+        }
+      },
+    );
+
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.purple,
-        ),
-        body: ListView(
-          children: genList.map((e) => Text(e)).toList(),
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.purple,
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                Text(
+                  "Current value is ${value.toString()}",
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text("Increment"),
+                  onPressed: () {
+                    ref.read(currentStateProvider.notifier).state += 1;
+                  },
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
